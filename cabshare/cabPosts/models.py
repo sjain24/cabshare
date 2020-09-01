46f5b94ebcfd0aec244c5b8a9ca53cf054bd79dc
AnamitraMandal <anamitramandal2008@gmail.com>
2020-09-01 19:01:03 +0530

    Minor changes

--- cabshare/cabPosts/models.py
+++ cabshare/cabPosts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.urls import reverse
-from users.models import User
+from django.contrib.auth import get_user_model as user_model
+User = user_model()
+#from users.models import User
 
 class Post(models.Model):
     whereFrom = models.CharField(max_length=255)
@@ -11,7 +13,7 @@ class Post(models.Model):
     flightOrTrainDetails = models.TextField(blank=True)
     spaceLeft=models.IntegerField(default=3)
     peopleInterested=models.IntegerField(default=0)
-    author=models.CharField(default=User.name)
+    author=models.ForeignKey(User , on_delete=models.CASCADE)
 
     def __str__(self):
         return self.whereFrom + '-' +self.whereTo + 'on '+ str(self.date)+ ' at '+str(self.time)
