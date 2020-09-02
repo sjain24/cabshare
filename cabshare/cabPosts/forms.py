cc39ea4792d0eb83899647c8da3c93c2172881f5
AnamitraMandal <anamitramandal2008@gmail.com>
2020-09-02 22:07:16 +0530

    Forms and views updated

--- cabshare/cabPosts/forms.py
+++ cabshare/cabPosts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from cabPosts.models import Post, Comment
+from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
@@ -8,16 +8,16 @@ class PostForm(forms.ModelForm):
     whereTo = forms.CharField(max_length=255)
     date = forms.DateField()
     time = forms.TimeField()
-    flightOrTrainDetails = forms.TextField(blank=True)
-    spaceLeft=forms.IntegerField(default=3)
-    peopleInterested=forms.IntegerField(default=0)
+    flightOrTrainDetails = forms.CharField(required=True)
+    spaceLeft=forms.IntegerField(initial=3)
+    #peopleInterested=forms.IntegerField(default=0)
 
     class Meta:
         model = Post
-        fields = {'whereFrom','whereTo', 'date','time','flightOrTrainDetails','spaceLeft',}
+        fields = {'whereFrom','whereTo', 'date','time','flightOrTrainDetails',}
 
 class CommentForm(forms.ModelForm):
-    content = forms.TextField(max_length=500)
+    content = forms.CharField(max_length=500)
 
     class Meta:
         model = Comment
--- cabshare/cabPosts/views.py
+++ cabshare/cabPosts/views.py
@@ -1,52 +1,83 @@
 from django.shortcuts import render
-from .models import Post, Comment
+#from .models import Post, Comment
 
-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 # Create your views here.import
-from cabPosts.forms import PostForm, CommentForm
-from cabPosts.models import Post, Comment
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-class HomeView(TemplateView):
-    template_name = './templates/pages/home.html'
+# class HomeView(TemplateView):
+#     template_name = 
 
-    def get(self, request):
-        postForm = PostForm()
-        current_user = request.user
-        posts = Post.objects.all().order_by('-date')
-        commentForm = CommentForm()
-        comments = Comment.objects.all()
-        args = {'postForm':postForm, 'posts':posts , 'users':users, 'commentForm':commentForm, 'comments':comments , }
+#     def get(self, request):
+#         postForm = PostForm()
+#         current_user = request.user
+#         posts = Post.objects.all().order_by('-date')
+#         commentForm = CommentForm()
+#         comments = Comment.objects.all()
+#         args = {'postForm':postForm, 'posts':posts , 'users':users, 'commentForm':commentForm, 'comments':comments , }
 
-        return render(request, self.template_name, args)
+#         return render(request, self.template_name, args)
 
-    def createNewPost(self , request):
-        postForm = PostForm(request.POST)
+#     def post(self , request):
+#         postForm = PostForm(request.POST)
+#         if postForm.is_valid():
+#             post = postForm.save(commit=False)
+#             post.author = request.user
+#             post.save()
+#             text = postForm.cleaned_data['post']
+#             postForm = PostForm()
+#             return redirect('home:home')
+
+#         args = {'postForm': postForm, 'text': text }
+#         return render(request, self.template_name,args)
+
+#     def createNewComment(self , request):
+#         commentForm = CommentForm(request.POST)
+#         if commentForm.is_valid():
+#             comment = commentForm.save(commit=False)
+#             comment.comment_author = request.user
+#           #?  comment.email = request.
+#             post.save()
+#             text = postForm.cleaned_data['post']
+#             postForm = PostForm()
+#             return redirect('home:home')
+
+#         args = {'postForm': postForm, 'text': text }
+#         return render(request, self.template_name,args)
+
+def get_posts(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        postForm=PostForm(request.POST)
         if postForm.is_valid():
-            post = postForm.save(commit=False)
-            post.author = request.user
-            post.save()
-            text = postForm.cleaned_data['post']
-            postForm = PostForm()
-            return redirect('home:home')
+            # post = postForm.save(commit=False)
+            # post.author = request.user
+            # post.save()
+            # text = postForm.cleaned_data['post']
+            # postForm = PostForm()
+            #return redirect('home:home')
 
-        args = {'postForm': postForm, 'text': text }
-        return render(request, self.template_name,args)
+        # check whether it's valid:
+        #if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        postForm = PostForm()
+        #current_user = request.user
+        posts = Post.objects.all()
+        #commentForm = CommentForm()
+        #comments = Comment.objects.all()
+        args = {'postForm':postForm, 'posts':posts ,}
 
-    def createNewComment(self , request):
-        commentForm = CommentForm(request.POST)
-        if commentForm.is_valid():
-            comment = commentForm.save(commit=False)
-            comment.comment_author = request.user
-          #?  comment.email = request.
-            post.save()
-            text = postForm.cleaned_data['post']
-            postForm = PostForm()
-            return redirect('home:home')
+    return render(request, 'cabPosts/posts.html', args)
 
-        args = {'postForm': postForm, 'text': text }
-        return render(request, self.template_name,args)
