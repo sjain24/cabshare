ff73351341a401ea7cdb75583ca65d0872e76269
sjain24 <siddharj@adobe.com>
2020-09-05 19:28:29 +0530

    minor changes in form structure

--- cabshare/cabPosts/forms.py
+++ cabshare/cabPosts/forms.py
@@ -1,22 +1,22 @@
 from django import forms
 from .models import Post, Comment
-
+from django.utils import timezone
 
 class PostForm(forms.ModelForm):
-    whereFrom = forms.CharField(max_length=255)
-    whereTo = forms.CharField(max_length=255)
-    date = forms.DateField()
-    time = forms.TimeField()
-    flightOrTrainDetails = forms.CharField(required=True)
-    spaceLeft=forms.IntegerField(initial=3)
+    whereFrom = forms.CharField(label="From", max_length=255, required = True)
+    whereTo = forms.CharField(label="To", max_length=255, required = True)
+    date = forms.DateField(initial=timezone.now(), widget = forms.SelectDateWidget, required = True)
+    time = forms.TimeField(initial=timezone.now(), required = True)
+    flightOrTrainDetails = forms.CharField(label = "Details", required = False)
+    spaceLeft=forms.IntegerField(initial=3, required = True, min_value = 0)
     #peopleInterested=forms.IntegerField(default=0)
-
+    field_order = ['whereFrom','whereTo', 'date','time','flightOrTrainDetails', 'spaceLeft',]
     class Meta:
         model = Post
         fields = {'whereFrom','whereTo', 'date','time','flightOrTrainDetails', 'spaceLeft',}
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(max_length=500)
+    content = forms.CharField(label="Add comment", max_length=500)
 
     class Meta:
         model = Comment
