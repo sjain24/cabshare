41d2126903f55ef017c2e5a78eda3f040698905d
AnamitraMandal <anamitramandal2008@gmail.com>
2020-09-08 08:45:24 +0530

    Delete post functionality added

--- cabshare/cabPosts/views.py
+++ cabshare/cabPosts/views.py
@@ -78,3 +78,43 @@ def get_search_results(request):
         else:
             return render(request, 'cabPosts/search.html', {'posts' : object_list ,'comments':comments,'commentForm':commentForm})
 
+@login_required
+def get_my_posts(request):
+    if request.method == 'POST':
+        if 'post' in request.POST:
+            postForm = PostForm(request.POST)
+            if postForm.is_valid():
+                post = postForm.save(commit=False)
+                post.author = request.user
+                post.save()
+                
+            return redirect('cabPosts:posts')
+        else:
+            commentForm=CommentForm(request.POST)
+            if commentForm.is_valid():
+                post_id = request.POST['post_id']
+                post_instance = get_object_or_404(Post, id=post_id)
+                comment = commentForm.save(commit=False)
+                comment.name = request.user
+                comment.post = post_instance
+                comment.email = request.user.email
+                comment.save()
+                return redirect('cabPosts:posts')
+            else:
+                return redirect('cabPosts:my_posts')
+
+    else:
+        postForm = PostForm()
+        posts = Post.objects.all()
+        commentForm = CommentForm()
+        current_user=request.user
+        comments=Comment.objects.all()
+        args = {'postForm':postForm, 'posts':posts ,'commentForm':commentForm,'comments':comments,'current_user':current_user}
+
+    return render(request, 'cabPosts/my_posts.html', args)
+def delete_my_post(request,key):
+    post = Post.objects.get(id=key)
+    print(post.id)
+    post.delete()
+    return redirect('cabPosts:my_posts_results')
+
